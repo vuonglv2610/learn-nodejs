b5a149d413ce691b0f5392890e1972de6332a66c
mume-vuonglv <vuonglv@mumesoft.vn>
2024-05-21 17:12:17 +0700

    add api remove and update product

--- src/controllers/product.controller.js
+++ src/controllers/product.controller.js
@@ -13,8 +13,9 @@ module.exports = {
 
   getOne: (req, res) => {
     ProductModel.getOne(req, res, (result) => {
-      if (!result) {
-        return Response.fail(req, res);
+      console.log(result);
+      if (!result || result.length === 0) {
+        return Response.fail(req, res, 404);
       }
       return Response.success(req, res, result);
     });
@@ -29,18 +30,34 @@ module.exports = {
     });
   },
 
-  edit: (req, res, result) => {
-    if (!result) {
-      return Response.fail(req, res);
-    }
-    return Response.success(req, res, result);
+  edit: (req, res) => {
+    ProductModel.edit(req, res, (result) => {
+      if (!result) {
+        return Response.fail(req, res);
+      }
+      return Response.success(req, res, result);
+    });
   },
 
-  remove: (req, res, result) => {
-    if (!result) {
-      return Response.fail(req, res);
-    }
-    return Response.success(req, res, result);
+  remove: (req, res) => {
+    ProductModel.remove(req, res, (result) => {
+      if (!result) {
+        return Response.fail(req, res);
+      }
+      return Response.success(req, res, result);
+    });
   },
 };
+
+
+
+
+
+
+
+
+
+
+
+
 
--- src/models/product.model.js
+++ src/models/product.model.js
@@ -3,7 +3,7 @@ const table = 'products';
 
 module.exports = {
   get: (req, res, result) => {
-    db.query(`SELECT * FROM ${table}`, (err, data) => {
+    db.query(`SELECT * FROM ${table} WHERE deleted_at IS NULL`, (err, data) => {
       if (err) return console.log(err);
       result(JSON.parse(JSON.stringify(data)));
     });
@@ -25,5 +25,80 @@ module.exports = {
       result(req.body);
     });
   },
+
+  edit: (req, res, result) => {
+    db.query(
+      `UPDATE ${table} SET name = ?, price = ?, description = ?, category_id = ? WHERE id = ${req.params.id}`,
+      [
+        req.body.name,
+        req.body.price,
+        req.body.description,
+        req.body.category_id,
+      ],
+      (err, data) => {
+        if (err) return console.log(err);
+        result(req.body);
+      }
+    );
+  },
+
+  remove: (req, res, result) => {
+    const now = new Date();
+    const day = String(now.getDate()).padStart(2, '0');
+    const month = String(now.getMonth() + 1).padStart(2, '0');
+    const year = now.getFullYear();
+    const formattedDate = `${day}/${month}/${year}`;
+
+    db.query(
+      `UPDATE ${table} SET deleted_at = ${formattedDate} WHERE id = ${req.params.id} AND deleted_at IS NULL`,
+      req.body,
+      (err, data) => {
+        if (err) return console.log(err);
+        result(req.body);
+      }
+    );
+  },
 };
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
