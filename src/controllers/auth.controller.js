b51daa46a2d4e9d9b098ee79b42fa3ae106111c7
namdd1710 <donam17102002@gmail.com>
2025-06-22 12:24:58 +0700

    remove log

--- src/controllers/auth.controller.js
+++ src/controllers/auth.controller.js
@@ -101,7 +101,6 @@ module.exports = {
       if (error.name === 'SequelizeDatabaseError') {
         return Response.fail(req, res, 400, 'Lỗi database');
       }
-      console.error('Login error:', error);
       return Response.fail(req, res, 500, 'Errors');
     }
   },
